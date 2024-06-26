2c12c09616d1a80398e471920fe0a2f3df800031
k-jdev <svv.ooj@gmail.com>
2024-06-26 21:46:13 +0300

    modified:   src/App.css 	modified:   src/App.jsx 	deleted:    src/components/Button/Button.css 	deleted:    src/components/Button/Button.jsx 	modified:   src/components/Task/Task.css 	modified:   src/components/Task/Task.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,42 +6,88 @@ function App() {
   const [text, setText] = React.useState("");
   const [tasks, setTasks] = React.useState([]);
 
-  // Функция удаления задачи
+  // delete task
   function removeTask(id) {
     setTasks((prevState) => prevState.filter((task) => task.id !== id));
   }
 
-  // Функция создания новой задачи
+  // toggle task completion
+  function toggleTaskCompletion(id) {
+    setTasks((prevState) =>
+      prevState.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  }
+
+  // create new task
   function createTask() {
     if (text.trim()) {
-      const newTask = { id: Date.now(), text: text };
+      const newTask = { id: Date.now(), text: text, completed: false };
       setTasks((prevState) => [...prevState, newTask]);
       setText("");
     }
   }
 
   return (
-    <div className="todo">
-      <h1>ToDo</h1>
-      <div className="todo__block">
-        <input
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-          type="text"
-        />
-        <button className="todo__send-btn" onClick={createTask}>
-          Send
-        </button>
+    <>
+      <div className="todo">
+        <h1>ToDo</h1>
+        <div className="todo__block">
+          <input
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+            type="text"
+          />
+          <button className="todo__send-btn" onClick={createTask}>
+            Send
+          </button>
+        </div>
+
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            id={task.id}
+            text={task.text}
+            completed={task.completed}
+            removeTask={removeTask}
+            toggleTaskCompletion={toggleTaskCompletion}
+          />
+        ))}
       </div>
-      {tasks.map((task) => (
-        <Task
-          key={task.id}
-          id={task.id}
-          text={task.text}
-          removeTask={removeTask}
-        />
-      ))}
-    </div>
+      <div className="todo__all-tasks">
+        <div className="todo__active-tasks">
+          <h2>Active tasks</h2>
+          {tasks
+            .filter((task) => !task.completed)
+            .map((task) => (
+              <Task
+                key={task.id}
+                id={task.id}
+                text={task.text}
+                completed={task.completed}
+                removeTask={removeTask}
+                toggleTaskCompletion={toggleTaskCompletion}
+              />
+            ))}
+        </div>
+        <div className="todo__complete-tasks">
+          <h2>Complete tasks</h2>
+          {tasks
+            .filter((task) => task.completed)
+            .map((task) => (
+              <Task
+                key={task.id}
+                id={task.id}
+                text={task.text}
+                completed={task.completed}
+                removeTask={removeTask}
+                toggleTaskCompletion={toggleTaskCompletion}
+              />
+            ))}
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Task.css";
 
-function Task({ id, text, removeTask }) {
+function Task({ id, text, completed, removeTask, toggleTaskCompletion }) {
   return (
-    <div className="task__block">
-      <input type="checkbox" />
-      <p className="task__text">{text}</p>
-      <button onClick={() => removeTask(id)}>Delete</button>
+    <div className={`task ${completed ? "completed" : ""}`}>
+      <span className="task-text">{text}</span>
+      <div className="buttons">
+        <button className="task-btn" onClick={() => toggleTaskCompletion(id)}>
+          {completed ? "Undo" : "Complete"}
+        </button>
+        <button onClick={() => removeTask(id)}>Delete</button>
+      </div>
     </div>
   );
 }
