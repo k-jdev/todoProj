99fa3c0572ab65dd702dd268f2a206591f77737c
k-jdev <svv.ooj@gmail.com>
2024-06-28 15:55:28 +0300

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Task from "./components/Task/Task";
 
@@ -6,12 +6,23 @@ function App() {
   const [text, setText] = React.useState("");
   const [tasks, setTasks] = React.useState([]);
 
-  // delete task
+  // Сохранение задач в localStorage при изменении задач
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  // Загрузка задач из localStorage при монтировании компонента
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    setTasks(storedTasks);
+  }, []);
+
+  // Удаление задачи
   function removeTask(id) {
     setTasks((prevState) => prevState.filter((task) => task.id !== id));
   }
 
-  // toggle task completion
+  // Переключение выполнения задачи
   function toggleTaskCompletion(id) {
     setTasks((prevState) =>
       prevState.map((task) =>
@@ -20,7 +31,7 @@ function App() {
     );
   }
 
-  // create new task
+  // Создание новой задачи
   function createTask() {
     if (text.trim()) {
       const newTask = { id: Date.now(), text: text, completed: false };
